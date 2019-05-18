9eaedb57c3281f4af9f2adfdc7b0e1865eeff376
huangchaobo <1798514453@qq.com>
2019-05-18 08:58:14 +0800

    删除了项目

--- test/OASys/src/com/chaoLiuWangLuo/OASys/service/attachmentService.java
+++
@@ -1,9 +0,0 @@
-package com.chaoLiuWangLuo.OASys.service;
-
-import com.chaoLiuWangLuo.OASys.entity.AoaAttachmentList;
-
-public interface attachmentService {
-
-	int addAttachment(AoaAttachmentList attachmentList);
-
-}
